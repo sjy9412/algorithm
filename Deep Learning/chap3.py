64e6962d4d86209f4a3b40ccc53b936d06fa7e48
sjy9412 <sjy9412@gmail.com>
2020-01-15 17:58:51 +0900

    Edit chap3

--- Deep Learning/chap3.py
+++ Deep Learning/chap3.py
@@ -1,6 +1,19 @@
 import numpy as np
+import matplotlib.pylab  as plt
 
+# 계단 함수
 def step_function(x):
     return np.array(x > 0, dtype=np.int)
 
-print(step_function())
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+print(y)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
+
+# 시그모이드 함수
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
