4fc2c264328eee1498b9302a167434a4f49f4e9f
sjy9412 <sjy9412@gmail.com>
2020-01-15 22:34:08 +0900

    Deep Learning | chap4

--- Deep Learning/chap3.py
+++ Deep Learning/chap3.py
@@ -5,15 +5,100 @@ import matplotlib.pylab  as plt
 def step_function(x):
     return np.array(x > 0, dtype=np.int)
 
-x = np.arange(-5.0, 5.0, 0.1)
-y = step_function(x)
-print(y)
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1)
-plt.show()
+# x = np.arange(-5.0, 5.0, 0.1)
+# y = step_function(x)
+# plt.plot(x, y)
+# plt.ylim(-0.1, 1.1)
+# plt.show()
 
 # 시그모이드 함수
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+# x = np.arange(-5.0, 5.0, 0.1)
+# y = sigmoid(x)
+# plt.plot(x, y)
+# plt.ylim(-0.1, 1.1)
+# plt.show()
 
+# 소프트맥스 함수
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+# 파일 불러오기
+import sys, os
+sys.path.append(os.pardir)
+from mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(flatten=True, normalize=False)
+
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
+
+# 이미지 불러오기
+sys.path.append(os.pardir)
+from PIL import Image
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint(img))
+    pil_img.show()
+
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(flatten=True, normalize=False)
+
+img = x_train[0]
+label = t_train[0]
+# print(label)
+# print(img.shape)
+img = img.reshape(28, 28)
+# print(img.shape)
+# img_show(img)
+
+# get_data
+def get_data():
+    (x_train, t_train), (x_test, t_test) = \
+        load_mnist(flatten=True, normalize=True, one_hot_label=False)
+    return x_test, t_test
+
+# init_network
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+# predict
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    z3 = sigmoid(a3)
+    y = softmax(a3)
+    return y
+
+# 정확도 평가 + 배치 처리
+import pickle
+x, t = get_data()
+network = init_network()
+
+batch_size = 100
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i + batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_cnt += np.sum(p == t[i:i + batch_size])
+
+print("Accuracy: " + str(float(accuracy_cnt) / len(x)))
---
+++ Deep Learning/chap4.py
@@ -0,0 +1,117 @@
+import numpy as np
+
+# 오차제곱합
+def sum_squares_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+# 교차 엔트로피 오차(미니 배치 + 원-핫 코딩)
+# def cross_entropy_error(y, t):
+#     if y.ndim == 1:
+#         t = t.reshape(1, t.size)
+#         y = y.reshape(1, y.size)
+#     batch_size = y.shape[0]
+#     return -np.sum(t * np.log(y + 1e-7)) / batch_size
+
+# 교차 엔트로피 오차(미니 배치)
+# def cross_entropy_error(y, t):
+#     if y.ndim == 1:
+#         t = t.reshape(1, t.size)
+#         y = y.reshape(1, y.size)
+#     batch_size = y.shape[0]
+#     return -np.sum(np.log(y[np.arrange(batch_size), t] + 1e-7)) / batch_size
+
+import sys, os
+sys.path.append(os.pardir)
+from functions import *
+from gradient import numerical_gradient
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        # 가중치 초기화
+        self.params = {}
+        self.params['W1'] = weight_init_std * \
+                            np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * \
+                            np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        return y
+    
+    # x: 입력 데이터, t: 정답 레이블
+    def loss(self, x, t):
+        y = self.predict(x)
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+
+# 시험 데이터로 평가하기
+from mnist import load_mnist
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(normalize=True, one_hot_label=True)
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+# 하이퍼파라미터
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+# 1에폭당 반복 수 
+iter_per_epoch = max(train_size / batch_size, 1)
+
+for i in range(iters_num):
+    # 미니배치 획득
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    # 기울기 계산
+    grad = network.numerical_gradient(x_batch, t_batch)
+
+    # 매개변수 갱신
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+    # 학습 경과 기록
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+    # 1에폭당 정확도 계산
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + ', ' + str(test_acc))
